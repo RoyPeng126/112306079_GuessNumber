a021b0b5099f32cbf37e666d21ebebd0b2cc9675
RoyPeng126 <147261778+RoyPeng126@users.noreply.github.com>
2023-12-29 13:07:56 +0800

    Initial commit

---
+++ guessNumber.js
@@ -0,0 +1,56 @@
+// Function to generate a random number between 1 and 10
+function generateRandomNumber() {
+  return Math.floor(Math.random() * 10) + 1;
+}
+
+// Function to show image and play sound
+function showImageAndPlaySound(imageSource, soundSource) {
+  let imageElement = document.createElement("img");
+  imageElement.src = imageSource;
+
+  let soundElement = document.createElement("audio");
+  soundElement.src = soundSource;
+  soundElement.autoplay = true;
+
+  let resultContainer = document.getElementById("result");
+  resultContainer.innerHTML = "";
+  resultContainer.appendChild(imageElement);
+  resultContainer.appendChild(soundElement);
+}
+
+// Function to check the user's guess
+function checkGuess() {
+  // Local variable
+  let userGuess = document.getElementById("userGuess").value;
+  let resultElement = document.getElementById("result");
+
+  // Generate a new random number for the next round
+  secretNumber = generateRandomNumber();
+
+  // Convert user input to a number
+  userGuess = parseInt(userGuess);
+
+  // Check if the guess is correct
+  if (userGuess === secretNumber) {
+    // If the answer is correct
+    resultElement.innerHTML = "Congratulations! You guessed the correct number!";
+    showImageAndPlaySound("picture2.jpeg", "music2.mp3");
+  } else {
+    // If the answer is wrong
+    resultElement.innerHTML = "Sorry, try again!";
+    showImageAndPlaySound("picture1.jpeg", "music1.mp3");
+  }
+
+  // Clear the input field for the next guess
+  document.getElementById("userGuess").value = "";
+}
+
+// Global variables
+let secretNumber = generateRandomNumber();
+
+// Add event listener for Enter key
+document.getElementById("userGuess").addEventListener("keyup", function (event) {
+  if (event.key === "Enter") {
+    checkGuess();
+  }
+});
